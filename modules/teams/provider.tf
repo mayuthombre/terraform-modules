e23f3bacaa8cfd70f9c6c732c489439296e05540
mayuthombre <mayu.thombre@furo.io>
2023-10-18 15:43:10 +1100

    revert changes

---
+++ modules/teams/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  # Configuration options
+  owner = var.owner_name
+  token = var.github_token
+}
--- modules/teams/variables.tf
+++ modules/teams/variables.tf
@@ -26,3 +26,13 @@ variable "parent_team_id" {
   type    = number
   default = null
 }
+
+variable "github_token" {
+  description = "GitHub access token used to configure the provider"
+  type        = string
+}
+
+variable "owner_name" {
+  description = "Oganisation name where team will be created"
+  type = string
+}
