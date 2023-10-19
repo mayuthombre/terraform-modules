6cbcfdfa94db253e7b0435d6f1ea7b441f39d617
mayuthombre <mayu.thombre@furo.io>
2023-10-19 16:24:47 +1100

    tf: try maintaining team using json

--- modules/teams/main.tf
+++ modules/teams/main.tf
@@ -1,12 +1,28 @@
+locals {
+  team_members = jsondecode(file(var.json_file))
+}
+
 resource "github_team" "team" {
   name           = var.name
   description    = var.description
   parent_team_id = var.parent_team_id
 }
 
+# resource "github_team_membership" "members" {
+#   for_each = { for member in var.members : member.username => member }
+#   team_id  = github_team.team.id
+#   username = each.value.username
+#   role     = each.value.role == null ? "member" : each.value.role
+# }
+
 resource "github_team_membership" "members" {
-  for_each = { for member in var.members : member.username => member }
+  for_each = {
+    for member in concat(
+      local.team_members.maintainers,
+      local.team_members.members
+    ) : member.username => member
+  }
   team_id  = github_team.team.id
-  username = each.value.username
-  role     = each.value.role == null ? "member" : each.value.role
+  username = each.key
+  role     = each.value.role
 }
--- modules/teams/variables.tf
+++ modules/teams/variables.tf
@@ -1,16 +1,21 @@
-variable "members" {
-  type = list(object({
-    username = string
-    role     = optional(string)
-  }))
-  default = []
-  description = <<EOT
-  List of members for the team and their role. For each member:
-    - username: Github username.
-    - role: (optional) the role granted to the team member. One of
-        - member (default)
-        - maintainer
-  EOT
+# variable "members" {
+#   type = list(object({
+#     username = string
+#     role     = optional(string)
+#   }))
+#   default = []
+#   description = <<EOT
+#   List of members for the team and their role. For each member:
+#     - username: Github username.
+#     - role: (optional) the role granted to the team member. One of
+#         - member (default)
+#         - maintainer
+#   EOT
+# }
+
+variable "json_file" {
+  description = "Path to the JSON file containing team members."
+  type        = string
 }
 
 variable "name" {
