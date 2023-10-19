24eece7443ed8b5982d4b3f775f1ad52d4faa88d
Sasidhar navuluri <sasidhar@Sasidhars-MBP.modem>
2023-10-19 17:02:35 +1100

    Add repository to teams

--- examples/teams/variables.tf
+++ examples/teams/variables.tf
@@ -1,19 +1,34 @@
-# variable "members" {
-#   type = list(object({
-#     username = string
-#     role     = optional(string)
-#   }))
-#   default = []
-#   description = <<EOT
-#   List of members for the team and their role. For each member:
-#     - username: Github username.
-#     - role: (optional) the role granted to the team member. One of
-#         - member (default)
-#         - maintainer
-#   EOT
-# }
-
 variable "github_token" {
-  description = "GitHub access token used to configure the provider"
+  description = "Token used to apply configuration changes"
   type        = string
+}
+
+variable "team_members" {
+  type = list(object({
+    username = string
+    role     = optional(string)
+  }))
+  default = []
+  description = <<EOT
+  List of members for the team and their role. For each member:
+    - username: Github username.
+    - role: (optional) the role granted to the team member. One of
+        - member (default)
+        - maintainer
+  EOT
+}
+
+variable "team_members_repos" {
+  type = list(object({
+    reponame = string
+    permission = optional(string)
+  }))
+  default = []
+  description = <<EOT
+  List of repos to be added for the team and its permissions. For each repo:
+    - reponame: Github repository name.
+    - permissions: Permission to be granted on that repository for the team
+        - pull (default)
+        - maintain
+  EOT
 }
