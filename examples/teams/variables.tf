8029c8d56111e986eb2a1c3b007bdb9684165507
Navuluri Sasidhar <83561759+NavuluriS@users.noreply.github.com>
2023-10-20 17:00:39 +1100

    Make all json objects as optional except name (#6)
    Co-authored-by: Sasidhar navuluri <sasidhar@Sasidhars-MBP.modem>

--- examples/teams/variables.tf
+++ examples/teams/variables.tf
@@ -2,8 +2,3 @@ variable "github_token" {
   description = "Token used to apply configuration changes"
   type        = string
 }
-
-variable "json_file" {
-  description = "Path to the JSON file containing team members."
-  type        = string
-}
