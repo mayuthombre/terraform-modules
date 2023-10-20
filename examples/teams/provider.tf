6834b124cb59c48fa6a05136eb998c1be76c725d
Navuluri Sasidhar <83561759+NavuluriS@users.noreply.github.com>
2023-10-20 14:55:02 +1100

    Add description variable (#5)
    * add description
    
    * add description 2
    
    * add description 3
    
    ---------
    
    Co-authored-by: Sasidhar navuluri <sasidhar@Sasidhars-MBP.modem>

--- examples/teams/provider.tf
+++ examples/teams/provider.tf
@@ -1,10 +1,10 @@
 terraform {
-#   backend "s3" {
-#     bucket         = "347000532987-organisation-team-config"
-#     key            = "furo-migrations-emu/furo-migrations-emu/terraform.tfstate"
-#     region         = "ap-southeast-1"
-#     dynamodb_table = "347000532987-organisation-team-config"
-#   }
+  #   backend "s3" {
+  #     bucket         = "347000532987-organisation-team-config"
+  #     key            = "furo-migrations-emu/furo-migrations-emu/terraform.tfstate"
+  #     region         = "ap-southeast-1"
+  #     dynamodb_table = "347000532987-organisation-team-config"
+  #   }
 
   required_providers {
     github = {
