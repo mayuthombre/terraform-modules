387c6225ef0e505a7650ae04526f2a26cca971e9
Sasidhar navuluri <sasidhar@Sasidhars-MBP.modem>
2023-10-20 14:50:27 +1100

    add description 3

--- modules/teams/main.tf
+++ modules/teams/main.tf
@@ -1,19 +1,19 @@
 locals {
   team_data = jsondecode(file(var.json_file))
 
-  team_name = local.team_data.team_name
+  team_name        = local.team_data.team_name
   team_description = local.team_data.description
 
   maintainers = local.team_data.maintainers
-  members = local.team_data.members
+  members     = local.team_data.members
 
-  admin_repos = local.team_data.repos.admin
+  admin_repos    = local.team_data.repos.admin
   maintain_repos = local.team_data.repos.maintain
-  push_repos = local.team_data.repos.push
-  triage_repos = local.team_data.repos.triage
+  push_repos     = local.team_data.repos.push
+  triage_repos   = local.team_data.repos.triage
 
-  uniq_members = distinct(concat(local.team_data.maintainers,local.team_data.members))
-  uniq_repos = distinct(concat(local.team_data.repos.admin,local.team_data.repos.maintain,local.team_data.repos.push,local.team_data.repos.triage))
+  uniq_members = distinct(concat(local.team_data.maintainers, local.team_data.members))
+  uniq_repos   = distinct(concat(local.team_data.repos.admin, local.team_data.repos.maintain, local.team_data.repos.push, local.team_data.repos.triage))
 
 }
 
@@ -35,5 +35,5 @@ resource "github_team_repository" "teams" {
   for_each   = toset(local.uniq_repos)
   team_id    = github_team.team.id
   repository = each.key
-  permission =  contains(local.admin_repos, each.key) ? "admin" : contains(local.maintain_repos, each.key) ? "maintain" : contains(local.push_repos, each.key) ? "push" : contains(local.triage_repos, each.key) ? "triage" : "pull"
+  permission = contains(local.admin_repos, each.key) ? "admin" : contains(local.maintain_repos, each.key) ? "maintain" : contains(local.push_repos, each.key) ? "push" : contains(local.triage_repos, each.key) ? "triage" : "pull"
 }
