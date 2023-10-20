9fb49374bfeb68adf8a63afc9990595538127efb
Navuluri Sasidhar <83561759+NavuluriS@users.noreply.github.com>
2023-10-20 14:55:02 +1100

    Add description variable (#5)
    * add description
    
    * add description 2
    
    * add description 3
    
    ---------
    
    Co-authored-by: Sasidhar navuluri <sasidhar@Sasidhars-MBP.modem>

--- modules/teams/main.tf
+++ modules/teams/main.tf
@@ -1,26 +1,26 @@
 locals {
   team_data = jsondecode(file(var.json_file))
 
-  team_name = local.team_data.team_name
+  team_name        = local.team_data.team_name
+  team_description = local.team_data.description
 
   maintainers = local.team_data.maintainers
-  members = local.team_data.members
+  members     = local.team_data.members
 
-  admin_repos = local.team_data.repos.admin
+  admin_repos    = local.team_data.repos.admin
   maintain_repos = local.team_data.repos.maintain
-  push_repos = local.team_data.repos.push
-  triage_repos = local.team_data.repos.triage
+  push_repos     = local.team_data.repos.push
+  triage_repos   = local.team_data.repos.triage
 
-  uniq_members = distinct(concat(local.team_data.maintainers,local.team_data.members))
-  uniq_repos = distinct(concat(local.team_data.repos.admin,local.team_data.repos.maintain,local.team_data.repos.push,local.team_data.repos.triage))
-  #uniq_repos = distinct(local.team_data.repos[*].*)
+  uniq_members = distinct(concat(local.team_data.maintainers, local.team_data.members))
+  uniq_repos   = distinct(concat(local.team_data.repos.admin, local.team_data.repos.maintain, local.team_data.repos.push, local.team_data.repos.triage))
 
 }
 
 
 resource "github_team" "team" {
   name           = local.team_name
-  description    = var.description
+  description    = local.team_description
   parent_team_id = var.parent_team_id # needs to be added in input json and get the id using data source
 }
 
@@ -35,5 +35,5 @@ resource "github_team_repository" "teams" {
   for_each   = toset(local.uniq_repos)
   team_id    = github_team.team.id
   repository = each.key
-  permission =  contains(local.admin_repos, each.key) ? "admin" : contains(local.maintain_repos, each.key) ? "maintain" : contains(local.push_repos, each.key) ? "push" : contains(local.triage_repos, each.key) ? "triage" : "pull"
+  permission = contains(local.admin_repos, each.key) ? "admin" : contains(local.maintain_repos, each.key) ? "maintain" : contains(local.push_repos, each.key) ? "push" : contains(local.triage_repos, each.key) ? "triage" : "pull"
 }
