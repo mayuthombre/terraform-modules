89cfd1965f102fdcb2a7f66286f2982ce7193eaf
Sasidhar navuluri <sasidhar@Sasidhars-MBP.modem>
2023-10-20 16:52:19 +1100

    Make all json objects as optional except name

--- modules/teams/main.tf
+++ modules/teams/main.tf
@@ -2,18 +2,18 @@ locals {
   team_data = jsondecode(file(var.json_file))
 
   team_name        = local.team_data.team_name
-  team_description = local.team_data.description
+  team_description = try(local.team_data.description, null)
 
-  maintainers = local.team_data.maintainers
-  members     = local.team_data.members
+  maintainers = try(local.team_data.maintainers, [])
+  members     = try(local.team_data.members, [])
 
-  admin_repos    = local.team_data.repos.admin
-  maintain_repos = local.team_data.repos.maintain
-  push_repos     = local.team_data.repos.push
-  triage_repos   = local.team_data.repos.triage
+  admin_repos    = try(local.team_data.repos.admin, [])
+  maintain_repos = try(local.team_data.repos.maintain, [])
+  push_repos     = try(local.team_data.repos.push, [])
+  triage_repos   = try(local.team_data.repos.triage, [])
 
-  uniq_members = distinct(concat(local.team_data.maintainers, local.team_data.members))
-  uniq_repos   = distinct(concat(local.team_data.repos.admin, local.team_data.repos.maintain, local.team_data.repos.push, local.team_data.repos.triage))
+  uniq_members = distinct(concat(local.maintainers, local.members))
+  uniq_repos   = distinct(concat(local.admin_repos, local.maintain_repos, local.push_repos, local.triage_repos))
 
 }
 
