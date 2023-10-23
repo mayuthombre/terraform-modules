e5656f53e918733aa7a134941e526ced99db4ccf
Navuluri Sasidhar <83561759+NavuluriS@users.noreply.github.com>
2023-10-23 06:02:20 +0000

    Revert "codeowners test"

--- examples/teams/teams.tf
+++ examples/teams/teams.tf
@@ -1,23 +1,7 @@
-module "team_black" {
+module "team_red" {
   #source       = "git::https://github.com/mayuthombre/terraform-modules.git//modules/teams?ref=tf/json"
   source       = "../../modules/teams"
-  json_file    = "team_black.json"
-  owner_name   = "furo-migrations-emu"
-  github_token = var.github_token
-}
-
-module "team_orange" {
-  #source       = "git::https://github.com/mayuthombre/terraform-modules.git//modules/teams?ref=tf/json"
-  source       = "../../modules/teams"
-  json_file    = "team_orange.json"
-  owner_name   = "furo-migrations-emu"
-  github_token = var.github_token
-}
-
-module "team_white" {
-  #source       = "git::https://github.com/mayuthombre/terraform-modules.git//modules/teams?ref=tf/json"
-  source       = "../../modules/teams"
-  json_file    = "team_white.json"
+  json_file    = "team_red.json"
   owner_name   = "furo-migrations-emu"
   github_token = var.github_token
 }
