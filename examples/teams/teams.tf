19b59a8f750898846a5e33f15319db4dff1ebdb3
Sasidhar navuluri <sasidhar@Sasidhars-MBP.modem>
2023-10-23 16:57:27 +1100

    codeowners test

--- examples/teams/teams.tf
+++ examples/teams/teams.tf
@@ -1,7 +1,23 @@
-module "team_red" {
+module "team_black" {
   #source       = "git::https://github.com/mayuthombre/terraform-modules.git//modules/teams?ref=tf/json"
   source       = "../../modules/teams"
-  json_file    = "team_red.json"
+  json_file    = "team_black.json"
+  owner_name   = "furo-migrations-emu"
+  github_token = var.github_token
+}
+
+module "team_orange" {
+  #source       = "git::https://github.com/mayuthombre/terraform-modules.git//modules/teams?ref=tf/json"
+  source       = "../../modules/teams"
+  json_file    = "team_orange.json"
+  owner_name   = "furo-migrations-emu"
+  github_token = var.github_token
+}
+
+module "team_white" {
+  #source       = "git::https://github.com/mayuthombre/terraform-modules.git//modules/teams?ref=tf/json"
+  source       = "../../modules/teams"
+  json_file    = "team_white.json"
   owner_name   = "furo-migrations-emu"
   github_token = var.github_token
 }
